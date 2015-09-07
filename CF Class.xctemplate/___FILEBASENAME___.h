6a7841b11094211acd7288418456231278b25781
Jeremy Agostino <jeremyagost@gmail.com>
2015-09-07 12:33:35 -0400

    CF Class template files

---
+++ CF Class.xctemplate/___FILEBASENAME___.h
@@ -0,0 +1,19 @@
+//
+//  ___FILENAME___
+//  ___PROJECTNAME___
+//
+//  Created by ___FULLUSERNAME___ on ___DATE___.
+//___COPYRIGHT___
+//
+
+#ifndef _____PROJECTNAMEASIDENTIFIER________FILEBASENAMEASIDENTIFIER_____
+#define _____PROJECTNAMEASIDENTIFIER________FILEBASENAMEASIDENTIFIER_____
+
+#include <CoreFoundation/CoreFoundation.h>
+
+typedef struct ___VARIABLE_productName___Class * ___VARIABLE_productName___Ref;
+
+extern CFTypeID
+___VARIABLE_productName___GetTypeID(void);
+
+#endif /* defined(_____PROJECTNAMEASIDENTIFIER________FILEBASENAMEASIDENTIFIER_____) */
---
+++ CF Class.xctemplate/___FILEBASENAME___Private.h
@@ -0,0 +1,31 @@
+//
+//  ___FILENAME___
+//  ___PROJECTNAME___
+//
+//  Created by ___FULLUSERNAME___ on ___DATE___.
+//___COPYRIGHT___
+//
+
+#ifndef _____PROJECTNAMEASIDENTIFIER________FILEBASENAMEASIDENTIFIER____PRIVATE__
+#define _____PROJECTNAMEASIDENTIFIER________FILEBASENAMEASIDENTIFIER____PRIVATE__
+
+#include "___FILEBASENAME___.h"
+#include "CFRuntime.h"	// Private CoreFoundation header that you will have to provide
+
+struct ___VARIABLE_productName___Class {
+
+	CFRuntimeBase base;
+
+	/*
+	 * Private members go here.
+	 */
+
+};
+
+extern void
+____VARIABLE_productName___ClassInitialize(void);
+
+extern struct ___VARIABLE_productName___Class *
+____VARIABLE_productName___CreateEmpty(void);
+
+#endif /* defined(_____PROJECTNAMEASIDENTIFIER________FILEBASENAMEASIDENTIFIER____PRIVATE__) */
